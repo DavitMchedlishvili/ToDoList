6d401772a784bc7f6a7a87bb878386d1cde8261c
DavitMchedlishvili <davit.mchedlishvili.2396@gmail.com>
2023-02-06 18:06:38 +0400

    to do list 1st time

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+
+function toDoList(){
+  
+    let item = document.getElementById("todoInput").value
+    let text = document.createTextNode(item)
+    let newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById("todoList").appendChild(newItem)
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
